b1fd337aa8c362fb1ebf7d7497cd8abc654be477
Ruben <ruben.cheyns@gmail.com>
2025-02-10 16:13:48 +0100

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -368,7 +368,7 @@ controller_1.buttonL1.pressed(onevent_controller_1buttonL1_pressed_0)
 controller_1.buttonL2.pressed(onevent_controller_1buttonL2_pressed_0)
 controller_1.buttonR1.pressed(onevent_controller_1buttonR1_pressed_0)
 controller_1.buttonR2.pressed(onevent_controller_1buttonR2_pressed_0)
-controller_1.buttonLeft.pressed(onevent_controller_1buttonLeft_pressed_0)
+controller_1.buttonUp.pressed(onevent_controller_1buttonUp_pressed_0)
 
 # add 15ms delay to make sure events are registered correctly.
 wait(15, MSEC)
